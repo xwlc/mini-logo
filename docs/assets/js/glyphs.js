bea55aa7e651aef7658f13b2e3961f8f3e06dd23
Charles Wong <charlie-wong@outlook.com>
2024-07-05 11:10:57 +0800

    docs: update comments only

--- docs/assets/js/glyphs.js
+++ docs/assets/js/glyphs.js
@@ -1,3 +1,8 @@
+// SPDX-License-Identifier: GPL-3.0-only OR Apache-2.0 OR MIT
+// SPDX-FileCopyrightText: 2024 Charles Wong <charlie-wong@outlook.com>
+// Created By: Charles Wong 2024-03-05T14:48:15+08:00 Asia/Shanghai
+// Repository: https://github.com/xwlc/mini-logo
+
 const FONTS = [
   "Abandon",
   "AnyFreak",
--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -1,4 +1,3 @@
-#!/usr/bin/env node
 // SPDX-License-Identifier: GPL-3.0-only OR Apache-2.0 OR MIT
 // SPDX-FileCopyrightText: 2024 Charles Wong <charlie-wong@outlook.com>
 // Created By: Charles Wong 2024-07-03T06:18:56+08:00 Asia/Shanghai
